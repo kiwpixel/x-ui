820245e6dc540f4c8240664215cdeb3a5c56d771
kiwpixel <151040350+kiwpixel@users.noreply.github.com>
2025-03-23 05:34:10 +0800

    Update web.go

--- web/web.go
+++ web/web.go
@@ -1,32 +1,32 @@
 package web
 
 import (
-	"context"
-	"crypto/tls"
-	"embed"
-	"github.com/BurntSushi/toml"
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-gonic/gin"
-	"github.com/nicksnyder/go-i18n/v2/i18n"
-	"github.com/robfig/cron/v3"
-	"golang.org/x/text/language"
-	"html/template"
-	"io"
-	"io/fs"
-	"net"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-	"x-ui/config"
-	"x-ui/logger"
-	"x-ui/util/common"
-	"x-ui/web/controller"
-	"x-ui/web/job"
-	"x-ui/web/network"
-	"x-ui/web/service"
+    "context"
+    "crypto/tls"
+    "embed"
+    "github.com/BurntSushi/toml"
+    "github.com/gin-contrib/sessions"
+    "github.com/gin-contrib/sessions/cookie"
+    "github.com/gin-gonic/gin"
+    "github.com/nicksnyder/go-i18n/v2/i18n"
+    "github.com/robfig/cron/v3"
+    "golang.org/x/text/language"
+    "html/template"
+    "io"
+    "io/fs"
+    "net"
+    "net/http"
+    "os"
+    "strconv"
+    "strings"
+    "time"
+    "x-ui/config"
+    "x-ui/logger"
+    "x-ui/util/common"
+    "x-ui/web/controller"
+    "x-ui/web/job"
+    "x-ui/web/network"
+    "x-ui/web/service"
 )
 
 //go:embed assets/*
@@ -41,354 +41,358 @@ var i18nFS embed.FS
 var startTime = time.Now()
 
 type wrapAssetsFS struct {
-	embed.FS
+    embed.FS
 }
 
 func (f *wrapAssetsFS) Open(name string) (fs.File, error) {
-	file, err := f.FS.Open("assets/" + name)
-	if err != nil {
-		return nil, err
-	}
-	return &wrapAssetsFile{
-		File: file,
-	}, nil
+    file, err := f.FS.Open("assets/" + name)
+    if err != nil {
+        return nil, err
+    }
+    return &wrapAssetsFile{
+        File: file,
+    }, nil
 }
 
 type wrapAssetsFile struct {
-	fs.File
+    fs.File
 }
 
 func (f *wrapAssetsFile) Stat() (fs.FileInfo, error) {
-	info, err := f.File.Stat()
-	if err != nil {
-		return nil, err
-	}
-	return &wrapAssetsFileInfo{
-		FileInfo: info,
-	}, nil
+    info, err := f.File.Stat()
+    if err != nil {
+        return nil, err
+    }
+    return &wrapAssetsFileInfo{
+        FileInfo: info,
+    }, nil
 }
 
 type wrapAssetsFileInfo struct {
-	fs.FileInfo
+    fs.FileInfo
 }
 
 func (f *wrapAssetsFileInfo) ModTime() time.Time {
-	return startTime
+    return startTime
 }
 
 type Server struct {
-	httpServer *http.Server
-	listener   net.Listener
+    httpServer *http.Server
+    listener   net.Listener
 
-	index  *controller.IndexController
-	server *controller.ServerController
-	xui    *controller.XUIController
+    index  *controller.IndexController
+    server *controller.ServerController
+    xui    *controller.XUIController
 
-	xrayService    service.XrayService
-	settingService service.SettingService
-	inboundService service.InboundService
+    xrayService    service.XrayService
+    settingService service.SettingService
+    inboundService service.InboundService
 
-	cron *cron.Cron
+    cron *cron.Cron
 
-	ctx    context.Context
-	cancel context.CancelFunc
+    ctx    context.Context
+    cancel context.CancelFunc
 }
 
 func NewServer() *Server {
-	ctx, cancel := context.WithCancel(context.Background())
-	return &Server{
-		ctx:    ctx,
-		cancel: cancel,
-	}
+    ctx, cancel := context.WithCancel(context.Background())
+    return &Server{
+        ctx:    ctx,
+        cancel: cancel,
+    }
 }
 
 func (s *Server) getHtmlFiles() ([]string, error) {
-	files := make([]string, 0)
-	dir, _ := os.Getwd()
-	err := fs.WalkDir(os.DirFS(dir), "web/html", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() {
-			return nil
-		}
-		files = append(files, path)
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return files, nil
+    files := make([]string, 0)
+    dir, _ := os.Getwd()
+    err := fs.WalkDir(os.DirFS(dir), "web/html", func(path string, d fs.DirEntry, err error) error {
+        if err != nil {
+            return err
+        }
+        if d.IsDir() {
+            return nil
+        }
+        files = append(files, path)
+        return nil
+    })
+    if err != nil {
+        return nil, err
+    }
+    return files, nil
 }
 
 func (s *Server) getHtmlTemplate(funcMap template.FuncMap) (*template.Template, error) {
-	t := template.New("").Funcs(funcMap)
-	err := fs.WalkDir(htmlFS, "html", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
+    t := template.New("").Funcs(funcMap)
+    err := fs.WalkDir(htmlFS, "html", func(path string, d fs.DirEntry, err error) error {
+        if err != nil {
+            return err
+        }
 
-		if d.IsDir() {
-			newT, err := t.ParseFS(htmlFS, path+"/*.html")
-			if err != nil {
-				// ignore
-				return nil
-			}
-			t = newT
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return t, nil
+        if d.IsDir() {
+            newT, err := t.ParseFS(htmlFS, path+"/*.html")
+            if err != nil {
+                // ignore
+                return nil
+            }
+            t = newT
+        }
+        return nil
+    })
+    if err != nil {
+        return nil, err
+    }
+    return t, nil
 }
 
 func (s *Server) initRouter() (*gin.Engine, error) {
-	if config.IsDebug() {
-		gin.SetMode(gin.DebugMode)
-	} else {
-		gin.DefaultWriter = io.Discard
-		gin.DefaultErrorWriter = io.Discard
-		gin.SetMode(gin.ReleaseMode)
-	}
+    if config.IsDebug() {
+        gin.SetMode(gin.DebugMode)
+    } else {
+        gin.DefaultWriter = io.Discard
+        gin.DefaultErrorWriter = io.Discard
+        gin.SetMode(gin.ReleaseMode)
+    }
 
-	engine := gin.Default()
+    engine := gin.Default()
 
-	secret, err := s.settingService.GetSecret()
-	if err != nil {
-		return nil, err
-	}
+    secret, err := s.settingService.GetSecret()
+    if err != nil {
+        return nil, err
+    }
 
-	basePath, err := s.settingService.GetBasePath()
-	if err != nil {
-		return nil, err
-	}
-	assetsBasePath := basePath + "assets/"
+    basePath, err := s.settingService.GetBasePath()
+    if err != nil {
+        return nil, err
+    }
+    assetsBasePath := basePath + "assets/"
 
-	store := cookie.NewStore(secret)
-	engine.Use(sessions.Sessions("session", store))
-	engine.Use(func(c *gin.Context) {
-		c.Set("base_path", basePath)
-	})
-	engine.Use(func(c *gin.Context) {
-		uri := c.Request.RequestURI
-		if strings.HasPrefix(uri, assetsBasePath) {
-			c.Header("Cache-Control", "max-age=31536000")
-		}
-	})
-	err = s.initI18n(engine)
-	if err != nil {
-		return nil, err
-	}
+    store := cookie.NewStore(secret)
+    engine.Use(sessions.Sessions("session", store))
+    engine.Use(func(c *gin.Context) {
+        c.Set("base_path", basePath)
+    })
+    engine.Use(func(c *gin.Context) {
+        uri := c.Request.RequestURI
+        if strings.HasPrefix(uri, assetsBasePath) {
+            c.Header("Cache-Control", "max-age=31536000")
+        }
+    })
+    err = s.initI18n(engine)
+    if err != nil {
+        return nil, err
+    }
 
-	if config.IsDebug() {
-		// for develop
-		files, err := s.getHtmlFiles()
-		if err != nil {
-			return nil, err
-		}
-		engine.LoadHTMLFiles(files...)
-		engine.StaticFS(basePath+"assets", http.FS(os.DirFS("web/assets")))
-	} else {
-		// for prod
-		t, err := s.getHtmlTemplate(engine.FuncMap)
-		if err != nil {
-			return nil, err
-		}
-		engine.SetHTMLTemplate(t)
-		engine.StaticFS(basePath+"assets", http.FS(&wrapAssetsFS{FS: assetsFS}))
-	}
+    if config.IsDebug() {
+        // for develop
+        files, err := s.getHtmlFiles()
+        if err != nil {
+            return nil, err
+        }
+        engine.LoadHTMLFiles(files...)
+        engine.StaticFS(basePath+"assets", http.FS(os.DirFS("web/assets")))
+    } else {
+        // for prod
+        t, err := s.getHtmlTemplate(engine.FuncMap)
+        if err != nil {
+            return nil, err
+        }
+        engine.SetHTMLTemplate(t)
+        engine.StaticFS(basePath+"assets", http.FS(&wrapAssetsFS{FS: assetsFS}))
+    }
 
-	g := engine.Group(basePath)
+    g := engine.Group(basePath)
 
-	s.index = controller.NewIndexController(g)
-	s.server = controller.NewServerController(g)
-	s.xui = controller.NewXUIController(g)
+    s.index = controller.NewIndexController(g)
+    s.server = controller.NewServerController(g)
+    s.xui = controller.NewXUIController(g)
 
-	return engine, nil
+    // 假设 a 是 InboundController 的实例，这里添加新的路由
+    a := s.xui.InboundController() // 假设存在这个方法获取 InboundController 实例
+    g.POST("/xui/inbound/addBatch", a.addBatchInbound)
+
+    return engine, nil
 }
 
 func (s *Server) initI18n(engine *gin.Engine) error {
-	bundle := i18n.NewBundle(language.SimplifiedChinese)
-	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
-	err := fs.WalkDir(i18nFS, "translation", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() {
-			return nil
-		}
-		data, err := i18nFS.ReadFile(path)
-		if err != nil {
-			return err
-		}
-		_, err = bundle.ParseMessageFileBytes(data, path)
-		return err
-	})
-	if err != nil {
-		return err
-	}
+    bundle := i18n.NewBundle(language.SimplifiedChinese)
+    bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+    err := fs.WalkDir(i18nFS, "translation", func(path string, d fs.DirEntry, err error) error {
+        if err != nil {
+            return err
+        }
+        if d.IsDir() {
+            return nil
+        }
+        data, err := i18nFS.ReadFile(path)
+        if err != nil {
+            return err
+        }
+        _, err = bundle.ParseMessageFileBytes(data, path)
+        return err
+    })
+    if err != nil {
+        return err
+    }
 
-	findI18nParamNames := func(key string) []string {
-		names := make([]string, 0)
-		keyLen := len(key)
-		for i := 0; i < keyLen-1; i++ {
-			if key[i:i+2] == "{{" { // 判断开头 "{{"
-				j := i + 2
-				isFind := false
-				for ; j < keyLen-1; j++ {
-					if key[j:j+2] == "}}" { // 结尾 "}}"
-						isFind = true
-						break
-					}
-				}
-				if isFind {
-					names = append(names, key[i+3:j])
-				}
-			}
-		}
-		return names
-	}
+    findI18nParamNames := func(key string) []string {
+        names := make([]string, 0)
+        keyLen := len(key)
+        for i := 0; i < keyLen-1; i++ {
+            if key[i:i+2] == "{{" { // 判断开头 "{{"
+                j := i + 2
+                isFind := false
+                for ; j < keyLen-1; j++ {
+                    if key[j:j+2] == "}}" { // 结尾 "}}"
+                        isFind = true
+                        break
+                    }
+                }
+                if isFind {
+                    names = append(names, key[i+3:j])
+                }
+            }
+        }
+        return names
+    }
 
-	var localizer *i18n.Localizer
+    var localizer *i18n.Localizer
 
-	engine.FuncMap["i18n"] = func(key string, params ...string) (string, error) {
-		names := findI18nParamNames(key)
-		if len(names) != len(params) {
-			return "", common.NewError("find names:", names, "---------- params:", params, "---------- num not equal")
-		}
-		templateData := map[string]interface{}{}
-		for i := range names {
-			templateData[names[i]] = params[i]
-		}
-		return localizer.Localize(&i18n.LocalizeConfig{
-			MessageID:    key,
-			TemplateData: templateData,
-		})
-	}
+    engine.FuncMap["i18n"] = func(key string, params ...string) (string, error) {
+        names := findI18nParamNames(key)
+        if len(names) != len(params) {
+            return "", common.NewError("find names:", names, "---------- params:", params, "---------- num not equal")
+        }
+        templateData := map[string]interface{}{}
+        for i := range names {
+            templateData[names[i]] = params[i]
+        }
+        return localizer.Localize(&i18n.LocalizeConfig{
+            MessageID:    key,
+            TemplateData: templateData,
+        })
+    }
 
-	engine.Use(func(c *gin.Context) {
-		accept := c.GetHeader("Accept-Language")
-		localizer = i18n.NewLocalizer(bundle, accept)
-		c.Set("localizer", localizer)
-		c.Next()
-	})
+    engine.Use(func(c *gin.Context) {
+        accept := c.GetHeader("Accept-Language")
+        localizer = i18n.NewLocalizer(bundle, accept)
+        c.Set("localizer", localizer)
+        c.Next()
+    })
 
-	return nil
+    return nil
 }
 
 func (s *Server) startTask() {
-	err := s.xrayService.RestartXray(true)
-	if err != nil {
-		logger.Warning("start xray failed:", err)
-	}
-	// 每 30 秒检查一次 xray 是否在运行
-	s.cron.AddJob("@every 30s", job.NewCheckXrayRunningJob())
+    err := s.xrayService.RestartXray(true)
+    if err != nil {
+        logger.Warning("start xray failed:", err)
+    }
+    // 每 30 秒检查一次 xray 是否在运行
+    s.cron.AddJob("@every 30s", job.NewCheckXrayRunningJob())
 
-	go func() {
-		time.Sleep(time.Second * 5)
-		// 每 10 秒统计一次流量，首次启动延迟 5 秒，与重启 xray 的时间错开
-		s.cron.AddJob("@every 10s", job.NewXrayTrafficJob())
-	}()
+    go func() {
+        time.Sleep(time.Second * 5)
+        // 每 10 秒统计一次流量，首次启动延迟 5 秒，与重启 xray 的时间错开
+        s.cron.AddJob("@every 10s", job.NewXrayTrafficJob())
+    }()
 
-	// 每 30 秒检查一次 inbound 流量超出和到期的情况
-	s.cron.AddJob("@every 30s", job.NewCheckInboundJob())
+    // 每 30 秒检查一次 inbound 流量超出和到期的情况
+    s.cron.AddJob("@every 30s", job.NewCheckInboundJob())
 }
 
 func (s *Server) Start() (err error) {
-	defer func() {
-		if err != nil {
-			s.Stop()
-		}
-	}()
+    defer func() {
+        if err != nil {
+            s.Stop()
+        }
+    }()
 
-	loc, err := s.settingService.GetTimeLocation()
-	if err != nil {
-		return err
-	}
-	s.cron = cron.New(cron.WithLocation(loc), cron.WithSeconds())
-	s.cron.Start()
+    loc, err := s.settingService.GetTimeLocation()
+    if err != nil {
+        return err
+    }
+    s.cron = cron.New(cron.WithLocation(loc), cron.WithSeconds())
+    s.cron.Start()
 
-	engine, err := s.initRouter()
-	if err != nil {
-		return err
-	}
+    engine, err := s.initRouter()
+    if err != nil {
+        return err
+    }
 
-	certFile, err := s.settingService.GetCertFile()
-	if err != nil {
-		return err
-	}
-	keyFile, err := s.settingService.GetKeyFile()
-	if err != nil {
-		return err
-	}
-	listen, err := s.settingService.GetListen()
-	if err != nil {
-		return err
-	}
-	port, err := s.settingService.GetPort()
-	if err != nil {
-		return err
-	}
-	listenAddr := net.JoinHostPort(listen, strconv.Itoa(port))
-	listener, err := net.Listen("tcp", listenAddr)
-	if err != nil {
-		return err
-	}
-	if certFile != "" || keyFile != "" {
-		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
-		if err != nil {
-			listener.Close()
-			return err
-		}
-		c := &tls.Config{
-			Certificates: []tls.Certificate{cert},
-		}
-		listener = network.NewAutoHttpsListener(listener)
-		listener = tls.NewListener(listener, c)
-	}
-	if certFile != "" || keyFile != "" {
-		logger.Info("web server run https on", listener.Addr())
-	} else {
-		logger.Info("web server run http on", listener.Addr())
-	}
-	s.listener = listener
+    certFile, err := s.settingService.GetCertFile()
+    if err != nil {
+        return err
+    }
+    keyFile, err := s.settingService.GetKeyFile()
+    if err != nil {
+        return err
+    }
+    listen, err := s.settingService.GetListen()
+    if err != nil {
+        return err
+    }
+    port, err := s.settingService.GetPort()
+    if err != nil {
+        return err
+    }
+    listenAddr := net.JoinHostPort(listen, strconv.Itoa(port))
+    listener, err := net.Listen("tcp", listenAddr)
+    if err != nil {
+        return err
+    }
+    if certFile != "" || keyFile != "" {
+        cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+        if err != nil {
+            listener.Close()
+            return err
+        }
+        c := &tls.Config{
+            Certificates: []tls.Certificate{cert},
+        }
+        listener = network.NewAutoHttpsListener(listener)
+        listener = tls.NewListener(listener, c)
+    }
+    if certFile != "" || keyFile != "" {
+        logger.Info("web server run https on", listener.Addr())
+    } else {
+        logger.Info("web server run http on", listener.Addr())
+    }
+    s.listener = listener
 
-	s.startTask()
+    s.startTask()
 
-	s.httpServer = &http.Server{
-		Handler: engine,
-	}
+    s.httpServer = &http.Server{
+        Handler: engine,
+    }
 
-	go func() {
-		s.httpServer.Serve(listener)
-	}()
+    go func() {
+        s.httpServer.Serve(listener)
+    }()
 
-	return nil
+    return nil
 }
 
 func (s *Server) Stop() error {
-	s.cancel()
-	s.xrayService.StopXray()
-	if s.cron != nil {
-		s.cron.Stop()
-	}
-	var err1 error
-	var err2 error
-	if s.httpServer != nil {
-		err1 = s.httpServer.Shutdown(s.ctx)
-	}
-	if s.listener != nil {
-		err2 = s.listener.Close()
-	}
-	return common.Combine(err1, err2)
+    s.cancel()
+    s.xrayService.StopXray()
+    if s.cron != nil {
+        s.cron.Stop()
+    }
+    var err1 error
+    var err2 error
+    if s.httpServer != nil {
+        err1 = s.httpServer.Shutdown(s.ctx)
+    }
+    if s.listener != nil {
+        err2 = s.listener.Close()
+    }
+    return common.Combine(err1, err2)
 }
 
 func (s *Server) GetCtx() context.Context {
-	return s.ctx
+    return s.ctx
 }
 
 func (s *Server) GetCron() *cron.Cron {
-	return s.cron
-}
+    return s.cron
+}    
